07582d7a5a91e146a7a9b3d197a87b4740634338
Trey Bertram <113378171+treybertram06@users.noreply.github.com>
2023-09-13 22:45:25 +0000

    edit

--- static/js.js
+++ static/js.js
@@ -10,3 +10,12 @@ function colourChange() {
     root.style.setProperty('--secondaryColour', primaryColor);
     root.style.setProperty('--textColour', textColor);
 };
+
+
+
+function moveDpad(dx, dy) {
+    var top = parseInt(dPad.style.top, 10);
+    var left = parseInt(dPad.style.left, 10);
+    dPad.style.top = (top + dy) + 'px';
+    dPad.style.left = (left + dx) + 'px';
+}
