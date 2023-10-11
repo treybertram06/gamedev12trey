186cc1b7a8fe3003b6ebfd6ee05d53edb872fa65
Trey Bertram <113378171+treybertram06@users.noreply.github.com>
2023-10-11 18:21:39 +0000

    edit

--- static/threeMain.js
+++ static/threeMain.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://threejs.org/build/three.module.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
