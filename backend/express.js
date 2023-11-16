6e2d5440fc29f17acf99b55842677dd02a98589f
Trey Bertram <113378171+treybertram06@users.noreply.github.com>
2023-11-16 18:52:43 +0000

    new

---
+++ backend/express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const hostname = '0.0.0.0'
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
